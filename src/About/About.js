fce9e0069fdd1b2865cd9ab0f1caa83fc22e3caf
adovg <you@example.com>
2020-09-20 21:29:10 +0300

    about module

---
+++ src/About/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './About.css'; 
+
+
+class About extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    render() {
+        return (           
+           <div>
+			   <p>Some content</p>
+		   </div>
+
+        );
+    }  
+}
+
+
+export default About;
